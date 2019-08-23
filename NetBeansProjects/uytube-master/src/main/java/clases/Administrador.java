a927c935570c3b762a4f323bcb47f510b9501ab6
Gabriel6211 <gabrielcastellano263@gmail.com>
2019-08-23 09:10:45 -0300

    Boton cerrar sesion

---
+++ NetBeansProjects/uytube-master/src/main/java/clases/Administrador.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author tesla
+ */
+
+
+public class Administrador extends Usuario{
+    
+    
+    
+    public Administrador(String nickname, String nombre, String apellido, String email, Date fechaNac, String imagen) {
+        super(nickname, nombre, apellido, email, fechaNac, imagen);
+    }
+   
+}
